b17bbed3cf4df084fb576d6962a7f08c296cf3db
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-04-14 19:16:30 -0300

    Add adjustments in Lexer and Program

---
+++ InterpreterForBasic.Domain/Entities/Lexer.cs
@@ -0,0 +1,35 @@
+﻿namespace InterpreterForBasic.Domain;
+
+public class Lexer
+{
+    public Dictionary<int, string> ProgramLines { get; private set; }
+
+    public Lexer()
+    {
+        ProgramLines = new Dictionary<int, string>();
+    }
+
+    public void Tokenize(string[] lines)
+    {
+        foreach (var line in lines)
+        {
+            ProcessLine(line);
+        }
+    }
+
+    private void ProcessLine(string line)
+    {
+        int firstSpaceIndex = line.IndexOf(' ');
+
+        if (firstSpaceIndex > 0)
+        {
+            string labelString = line.Substring(0, firstSpaceIndex);
+
+            if (int.TryParse(labelString, out int lineNumber))
+            {
+                string content = line.Substring(firstSpaceIndex + 1).Trim();
+                ProgramLines[lineNumber] = content;
+            }
+        }
+    }
+}
--- InterpreterForBasic/Program.cs
+++ InterpreterForBasic/Program.cs
@@ -1,9 +1,9 @@
-﻿namespace InterpreterForBasic;
+﻿using InterpreterForBasic.Domain;
+
+namespace InterpreterForBasic;
 
 internal class Program
 {
-    static Dictionary<int, string> labelTable = new Dictionary<int, string>();
-
     static void Main(string[] args)
     {
         string filePath = "assets/file/example.basic";
@@ -13,6 +13,14 @@ internal class Program
             if (File.Exists(filePath))
             {
                 string[] lines = File.ReadAllLines(filePath);
+
+                Lexer lexer = new Lexer();
+                lexer.Tokenize(lines);
+
+                foreach (var entry in lexer.ProgramLines)
+                {
+                    Console.WriteLine($"Line {entry.Key}: {entry.Value}");
+                }
             }
             else
             {
